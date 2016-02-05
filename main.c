8bd1f29755ec3adf6fbb6f6b82c6fb06c2c744a8
luca998 <luca.caracciolo16@gmail.com>
2016-02-05 17:25:59 +0100

    main.c

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-char* welcome();
-int main ()
-{
-char s;
-printf ("%s",welcome());
+ 
+ char* welcome();
+ int main (){
+    char s;
+    printf ("%s",welcome());
 
-return 0;
-}
-char* welcome()
-{
-return "HELLO\n";
-}
+    return 0;
+  }
+ 
+ char* welcome()
+ {
+ return "HELLO\n";
+  }
 
