bc2a1b7f002146bd6ce77813371b8588e0d5db5e
Fabio Z Tessitore <fabioztessitore@gmail.com>
2016-02-07 19:13:10 +0100

    little fix

--- main.c
+++ main.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
  
- char* welcome();
- int main (){
-    char s;
-    printf ("%s",welcome());
+char* welcome();
 
-    return 0;
-  }
- 
- char* welcome()
- {
- return "*** Welcome to Simpletron! ***\n"
-        "*** Please enter your program one instruction ***\n"
-        "*** (or data word) at a time. I will type the ***\n"
-        "*** location number and a question mark (?).  ***\n"
-        "*** You then type the word for that location. ***\n"
-        "*** Type the sentinel -99999 to stop entering ***\n"
-        "*** your program. ***\n";
+int main ()
+{
+  printf ("%s", welcome());
+
+  return 0;
+}
+
+char* welcome()
+{
+return "*** Welcome to Simpletron! ***\n"
+       "*** Please enter your program one instruction ***\n"
+       "*** (or data word) at a time. I will type the ***\n"
+       "*** location number and a question mark (?).  ***\n"
+       "*** You then type the word for that location. ***\n"
+       "*** Type the sentinel -99999 to stop entering ***\n"
+       "*** your program. ***\n";
 }
