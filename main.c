e93956d7216d1a17df8d36edc6ae33fc66e35134
luca998 <luca.caracciolo16@gmail.com>
2016-02-07 19:08:27 +0100

    main.c

--- main.c
+++ main.c
@@ -10,6 +10,11 @@
  
  char* welcome()
  {
- return "HELLO\n";
-  }
-
+ return "*** Welcome to Simpletron! ***\n"
+        "*** Please enter your program one instruction ***\n"
+        "*** (or data word) at a time. I will type the ***\n"
+        "*** location number and a question mark (?).  ***\n"
+        "*** You then type the word for that location. ***\n"
+        "*** Type the sentinel -99999 to stop entering ***\n"
+        "*** your program. ***\n";
+}
